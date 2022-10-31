5a374c749a1094fd1819b6c70e6c26ab01742d07
Melissa Konig <melissa.konig@proway.treina>
2022-10-31 19:16:24 -0300

    atualização parte Mel

--- main.py
+++ main.py
@@ -9,12 +9,24 @@ from controller import *
 
 
 def menu():
-    
-    while True:
-        print('-'* 30)
-        
+
+    print('''
+        Seja bem-vindo! Como gostaria de prosseguir?
+        [A] - Cadastrar Aluno.
+        [B] - Relatório de Alunos.
+    ''')
+
+    escolha = str(input('Escolha uma das opções listadas: '))
+
+    cadastroAluno = "A"
+    relatorioAlunos = "B"
+
+    if escolha == "A":
         dados_aluno = {}
         dados_aluno["nome"] = input(("Digite seu nome: "))
         dados_aluno["sobrenome"] = input(("Digite seu sobrenome: "))
         dados_aluno["idade"] = input(("Digite sua idade: "))
         salvar_alunos(dados_aluno)
+
+    if escolha == "B":
+        print(listarAlunos())
