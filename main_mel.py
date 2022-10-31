0a0dea1c0c194d6bf41e62a07b5e4cd32d2b6281
Melissa Konig <melissa.konig@proway.treina>
2022-10-31 20:23:18 -0300

    Atualizado

--- main_mel.py
+++ main_mel.py
@@ -1,13 +1,14 @@
 """
-Criar cadastro para alunos e depois listar os cadastros.
+Parte Mel:
+1. Criar cadastro Alunos.
+2. Reproduzir listagem Alunos.
 
 """
-from controllerMel import *
+#Import das funções constantes no controller.
+from controllerMel import salvarAlunos, listarAlunos
 
 
-
-
-
+#Função menu de alunos (única que consta na main)
 def menu():
 
     print('''
@@ -15,22 +16,25 @@ def menu():
         [A] - Cadastrar Aluno.
         [B] - Relatório de Alunos.
     ''')
-
-    escolha = str(input('Escolha uma das opções listadas: '))
-
+    
     cadastroAluno = "A"
     relatorioAlunos = "B"
 
+    escolha = str(input('Escolha uma das opções listadas: '))
+
+    #Dicionário save cadastro alunos.
     if escolha == "A":
         dados_aluno = {}
-        dados_aluno["nome"] = input(("Digite o nome completo do aluno: "))
-        dados_aluno["sobrenome"] = input(("Digite o ano em que o aluno se encontra: "))
-        dados_aluno["idade"] = input(("Digite a matrícula do aluno: "))
-        dados_aluno["notas"] = ()
+        dados_aluno["nome"] = input(str("Digite o nome completo do aluno: "))
+        dados_aluno["ano"] = input(str("Digite o ano em que o aluno se encontra: "))
+        dados_aluno["matricula"] = input(int("Digite a matrícula do aluno: "))
+        dados_aluno["CPF"] = input(int("Digite o CPF do aluno: "))
         salvarAlunos(dados_aluno)
+    
     print("Aluno cadastrado!")
 
+    #Escolha do usuário para listagem dos alunos cadastrados.
     if escolha == "B":
         print(listarAlunos())
     
-menu()
+menu() #Fim função menu.
