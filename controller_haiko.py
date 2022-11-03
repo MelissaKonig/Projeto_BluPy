bac0bcd78ff1c8ec1283cd9797e313d138943a4e
Everton Denega Souza <everton.souza@proway.treina>
2022-11-03 18:48:11 -0300

    deletando arquivos antigos

--- controller_haiko.py
+++
@@ -1,7 +0,0 @@
-def salvar_professor(professores):#criando funcao e pegando os dados dos professores
-    with open('dadosProfessor.txt', 'a') as arquivo: #abrindo o arquivo txt
-        arquivo.write(f'{professores}\n') #salvando tudo no arquivo txt os dados que tinham no professor
-
-def exibir(): #criando uma funcao 
-    with open('dadosProfessor.txt', 'r') as arquivo: #abrindo o arquivo txt
-         print(arquivo.read())# exibindo todas as informacao do arquivo txt
--- funcaoes.py
+++
@@ -1,7 +0,0 @@
-def salvar_professor(professores):
-    with open('dadosProfessor.txt', 'a') as arquivo:
-        arquivo.write(f'{professores}\n')
-
-def exibir():
-    with open('dadosProfessor.txt', 'r') as arquivo:
-         print(arquivo.read())
